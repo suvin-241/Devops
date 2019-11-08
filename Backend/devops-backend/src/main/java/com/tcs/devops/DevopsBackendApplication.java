1ef3f0bb062811f8f587a7fefce9ff6bacc51c99
suvin-241 <suvin241@gmail.com>
2019-11-08 22:58:06 +0000

    initial cmmit

---
+++ Backend/devops-backend/src/main/java/com/tcs/devops/DevopsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsBackendApplication.class, args);
+	}
+
+}
