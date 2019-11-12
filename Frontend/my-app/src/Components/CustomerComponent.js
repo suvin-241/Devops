f8c6a6df677d41e50837924407600f6d985aff95
suvin-241 <suvin241@gmail.com>
2019-11-12 12:01:42 +0000

    ui changes

--- Frontend/my-app/src/Components/CustomerComponent.js
+++ Frontend/my-app/src/Components/CustomerComponent.js
@@ -1,6 +1,31 @@
 import React from "react";
 import { callGet } from "../service/ApiService";
 import { GET_CUSTOMER } from "../common/Urlconstants";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+
+
+const StyledTableCell = withStyles(theme => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+
+  const StyledTableRow = withStyles(theme => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.background.default,
+      },
+    },
+  }))(TableRow);
 
 
 class CustomerComponent extends React.Component{
@@ -21,7 +46,35 @@ componentDidMount(){
     render(){
         return(
 <div>
-    Hello world {this.state.customerdata && this.state.customerdata[0].firstname}
+<Table className="custom-table" styles={{width:"75%"}} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+          <StyledTableCell>Customer ID</StyledTableCell>
+            <StyledTableCell align="right">Title</StyledTableCell>
+            <StyledTableCell align="right">First Name</StyledTableCell>
+            <StyledTableCell align="right">Last Name</StyledTableCell>
+            <StyledTableCell align="right">Email</StyledTableCell>
+            <StyledTableCell align="right">Mobile Number</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.state.customerdata && this.state.customerdata.map(row => (
+            <StyledTableRow key={row.customerid}>
+              <StyledTableCell component="th" scope="row">
+                  {row.customerid}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.title}</StyledTableCell>
+              <StyledTableCell align="right">{row.firstname}</StyledTableCell>
+              <StyledTableCell align="right">{row.lastname}</StyledTableCell>
+              <StyledTableCell align="right">{row.email}</StyledTableCell>
+              <StyledTableCell align="right">{row.mobileno}</StyledTableCell>
+
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    
+    
 </div>
 
         );
