787961ac3f0f0431607db65e0b49aef00d16802f
suvin-241 <suvin241@gmail.com>
2019-11-13 09:50:54 +0000

    add

---
+++ Backend/devops-backend/src/main/java/com/suvin/common/GenericDao.java
@@ -0,0 +1,27 @@
+package com.suvin.common;
+
+
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface GenericDao<T extends Serializable> {
+
+	T find(Object id);
+
+	T save(T t);
+
+	void delete(Object id);
+
+	T update(T t);
+
+	List<T> findAllAsList();
+
+	void batchSave(List<T> list);
+
+	void bacthupdate(List<T> list);
+
+	void batchDelete(List<T> list);
+
+
+}
